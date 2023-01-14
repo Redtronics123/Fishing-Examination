5e49ca793126801509af2221a41f473cf868d796
Redtronics <development@redtronics.de>
2023-01-14 15:01:10 +0100

    update path for a short time

--- database/connect.py
+++ database/connect.py
@@ -3,7 +3,7 @@ import sqlite3
 
 class Database:
     def __init__(self):
-        self.connection = sqlite3.connect("../ressourcen/databases/brandenburg.db")
+        self.connection = sqlite3.connect("E:\Jetbrains Projekts\PyCharm\examinationFishing\\ressourcen\databases\\brandenburg.db")
 
     def connect(self):
         return self.connection
